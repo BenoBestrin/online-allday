81a2a8fc2da4125838f14551b9328ff2037a7433
beno bestrin <beno.patrol@gmail.com>
2022-09-22 00:41:22 -0300

    press a key

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pyautogui
 import time
 import datetime
+from pyautogui import press, typewrite, hotkey
 
 
-def move_cursor():
+
+def press_key():
 
    is_active_loop = True
    print("Welcome to  run cursor automatic")
@@ -11,12 +12,12 @@ def move_cursor():
        now = datetime.datetime.now()
        print("Last run in : ")
        print(now)
-       pyautogui.move(0, 10)
+       press('q')
        time.sleep(5)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-   move_cursor()
+   press_key()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
